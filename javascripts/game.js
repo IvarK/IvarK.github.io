b7bdb0a08048ad954261dad3afaca2d42a5a61c2
IvarK <IvarK@users.noreply.github.com>
2017-08-27 22:11:09 +0300

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -1174,7 +1174,7 @@ function sacrifice() {
 
 document.getElementById("sacrifice").onclick = function() {
   if (document.getElementById("confirmation").checked && player.eightAmount != 0) sacrifice();
-  else if (confirm("Dimensional Sacrifice will sacrifice your dimensions 1-7, but you will get a boost to Eighth Dimension. THIS MIGHT AFFECT YOUR PROGRESS NEGATIVELY.") && player.eightAmount != 0) sacrifice();
+  else if (confirm("Dimensional Sacrifice will reduce the amount of dimensions from 1 to 7 to 0, but the cost and the multiplier stays the same, you will also get a boost to Eighth Dimension. THIS MIGHT AFFECT YOUR PROGRESS NEGATIVELY.") && player.eightAmount != 0) sacrifice();
   
 }
 
