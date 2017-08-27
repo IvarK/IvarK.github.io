dbb82c86a475e5131f88d131908c49d9613c3095
Kyle <Kyle12qw@gmail.com>
2017-08-27 19:57:52 -0400

    Update util.js
    Moved the get_cookie function to the util file as it is a utility function.

--- javascripts/util.js
+++ javascripts/util.js
@@ -1,1 +1,15 @@
-
+function get_cookie(cookie_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + cookie_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(cookie_name + "=");
+    }
+    if (c_start == -1) return false;
+    c_start = c_value.indexOf("=", c_start) + 1;
+    var c_end = c_value.indexOf(";", c_start);
+    if (c_end == -1) {
+        c_end = c_value.length;
+    }
+    c_value = atob(unescape(c_value.substring(c_start,c_end)));
+    return JSON.parse(c_value);
+}
