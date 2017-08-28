7f6d3caf2474a36cbd7e0f2c40f544424f2b2d41
Kyle <Kyle12qw@gmail.com>
2017-08-27 20:11:58 -0400

    Update util.js
    Moved load_game function to the util file as it is a utility function.

--- javascripts/util.js
+++ javascripts/util.js
@@ -21,3 +21,56 @@ function get_cookie(cookie_name) {
     c_value = atob(unescape(c_value.substring(c_start,c_end)));
     return JSON.parse(c_value);
 }
+
+function load_game() {
+    var save_data = get_cookie('dimensionSave');
+      if (!save_data) return;
+    	player = save_data;
+      if (player.totalmoney === undefined) player.totalmoney = player.money;
+      if (player.options === undefined) {
+        player.options = {
+			    scientific: false,
+			    animationOn: true
+      	}
+      }
+      if (player.options.invert === undefined) player.options.invert = false;
+      if (player.options.logoVisible === undefined) player.options.logoVisible = true;
+      if (player.achievements === undefined) player.achievements = []; 
+      if (player.sacrificed === undefined) player.sacrificed = 0;
+	    if (player.infinityUpgrades === undefined) player.infinityUpgrades = [];
+      if (player.infinityPoints === undefined) player.infinityPoints = 0;
+	    if (player.infinitied === undefined) player.infinitied = 0;
+	    if (player.totalTimePlayed === undefined) player.totalTimePlayed = 0;
+	    if (player.bestInfinityTime === undefined) player.bestInfinityTime = 9999999999;
+	    if (player.thisInfinityTime === undefined) player.thisInfinityTime = 9999999999;
+	    if (player.galaxies === undefined) player.galaxies = 0;
+      if (player.lastUpdate === undefined) player.lastUpdate = new Date().getTime();
+    	if (player.firstAmount !== 0) document.getElementById("secondRow").style.display = "table-row";
+    	if (player.secondAmount !== 0) { 
+    	document.getElementById("thirdRow").style.display = "table-row";
+  		document.getElementById("tickSpeed").style.visibility = "visible";
+      document.getElementById("tickSpeedMax").style.visibility = "visible";
+  		document.getElementById("tickLabel").style.visibility = "visible";
+  		document.getElementById("tickSpeedAmount").style.visibility = "visible";
+    	}
+	
+    	if (player.thirdAmount !== 0) document.getElementById("fourthRow").style.display = "table-row";
+    	if (player.fourthAmount !== 0) if (player.resets > 0) document.getElementById("fifthRow").style.display = "table-row";
+    	if (player.fifthAmount !== 0) if (player.resets > 1) document.getElementById("sixthRow").style.display = "table-row";
+    	if (player.sixthAmount !== 0) if (player.resets > 2) document.getElementById("seventhRow").style.display = "table-row";
+    	if (player.seventhAmount !== 0) if (player.resets > 3) document.getElementById("eightRow").style.display = "table-row";
+    	updateCosts();
+      updateTickSpeed();
+      
+      var achievements = document.getElementsByClassName('achievement');
+      var achievement;
+      for (var i = 0; i < achievements.length; i++) {
+        achievement = achievements.item(i);
+        if (player.achievements.includes(achievement.id)) {
+            achievement.className = 'achievement achievementunlocked';
+        } else {
+						achievement.className = 'achievement achievementlocked';
+	}
+      }
+      
+}
