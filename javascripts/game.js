ac501a52f29c917339bf7b4706d0ad0ee7507192
Some One <1234abcdcba4321@users.noreply.github.com>
2017-09-03 18:22:59 -0500

    Update game.js
    whyd you ask me to do this wtf

--- javascripts/game.js
+++ javascripts/game.js
@@ -1204,7 +1204,7 @@ document.getElementById("sacrifice").onclick = function () {
     }
     
     if (!document.getElementById("confirmation").checked) {
-        if (!confirm("Dimensional Sacrifice will reduce the amount of dimensions from 1 to 7 to 0, but the cost and the multiplier stays the same, you will also get a boost to Eighth Dimension. THIS MIGHT AFFECT YOUR PROGRESS NEGATIVELY.")) {
+        if (!confirm("Dimensional Sacrifice will remove all of your first to seventh dimensions (with the cost and multiplier unchanged) for a boost to Eighth Dimension. It will take time to regain production.")) {
             return false;
         }
     }
