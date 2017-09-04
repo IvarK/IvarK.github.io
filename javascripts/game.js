1193ac85f4a286b9cc24dde2d5ef458a48a9ba3c
IvarK <ivar.kerajarvi@aalto.fi>
2017-09-05 02:11:22 +0300

    small fixes

--- javascripts/game.js
+++ javascripts/game.js
@@ -398,9 +398,9 @@ function updateTickSpeed() {
 
 
 function updateChallenges() {
-  
-  for (challenge in document.getElementsByClassName('onchallengebtn')) {
-    challenge.className = "challengesbtn";
+  var buttons = document.getElementsByClassName('onchallengebtn')
+  for (var i=0; i < buttons.length; i++) {
+    buttons[i].className = "challengesbtn";
   }
   
   for (challenge in player.challenges) {
@@ -2411,7 +2411,13 @@ var newsArray = ["You just made your 1,000,000,000,000,000 antimatter. This one 
 "This world situation is a SOS situation to the world!! MAYDAY, MAYDAY!!", "As for sure as the sun rises in the west, of all the singers and poets on earth, I am the bestest. - hevipelle", "I'm good at using github -hevipelle",
 "A new chat server has been created for Antimatter people to spy on Matter people, and the world has fallen into chaos and discord", "A new study has come out linking the consumption of potatoes with increased risk of Antimatter implosion.  Scientists suggest eating more.",
 "A new group for the standardisation of numbers have come forward with a novel new format involving emoji's.", "I thought that I fixed that bug but apparently some update broke it again -hevipelle",
-]
+"Maybe I'm gay then -Bootato", "Breaking news! Hevipelle has just announced that the buy max button is in fact going to be removed!", "I dedicate this game to my girlfriend", 
+"Antimatter guns don't kill antimatter people, antimatter people kill antimatter people but does that mean that antimatter toaster doesn't toast antimatter toasts, antimatter toast toasts antimatter toasts?", 
+"But to an antimatter person, wouldn't they be matter and us antimatter?", "And nothing Antimatters", "Why is there no 9th dimension? Because 7 8 9.", 
+"School starting up strikes fear in students universe-wide, as schools are no longer segregated between Matter and antimatter. Annihilation is prominent.",
+"Why does no one talk about the 0th dimension?", "Antimatter ice cream stand has recently opened- they have octillions of flavors!", "The 9th dimension cannot exist because the Nein-speaking nazis died in WW2.",
+"The fatter catter satter on the antimatter.", "Who let the DOgs out?", "I've got 1.79e308 problems, but none of them antimatters", "If you can't read this you disabled the news.", 
+"Doesn't leave, just mutes the server so he doesn't receive notifications", "Most quotes found online are falsely atributed -Abraham Lincoln"]
 
 
 var conditionalNewsArray = ["Our universe is falling apart. We are all evacuating. This is the last news cast", "THIS NEWS STATION HAS SHUT DOWN DUE TO COLLAPSING UNIVERSE", 
@@ -2506,6 +2512,7 @@ function init() {
     showTab('infinity')
     load_game();
     updateTickSpeed();
+	updateAutobuyers();
     if (!player.options.animationsOn) document.getElementById("logoanimation").src = "animation.png";
     if (player.options.invert) {
         document.getElementById("body").classList.add("invert");
