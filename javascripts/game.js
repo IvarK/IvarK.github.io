bba203e9e75a683a7c94ca00a6d36e26dcc28950
IvarK <ivar.kerajarvi@aalto.fi>
2017-09-06 21:17:17 +0300

    fixes, save system change, achievement images

--- javascripts/game.js
+++ javascripts/game.js
@@ -84,26 +84,27 @@ var eightButton = document.getElementById("eight");
 var tickSpeedButton = document.getElementById("tickSpeed");
 
 function set_cookie(cookie_name, value) {
-    expiry = new Date();
-    expiry.setTime(new Date().getTime() + (365 * 24 * 60 * 60 * 1000));
-    var c_value = escape(btoa(JSON.stringify(value, function(k, v) { return (v === Infinity) ? "Infinity" : v; }))) +
-        "; expires=" + expiry.toUTCString();
-    document.cookie = cookie_name + "=" + c_value;
+    localStorage[cookie_name] = btoa(JSON.stringify(value))
 }
 
 function get_cookie(cookie_name) {
     var c_value = document.cookie;
-    var c_start = c_value.indexOf(" " + cookie_name + "=");
-    if (c_start == -1) {
-        c_start = c_value.indexOf(cookie_name + "=");
+    if (localStorage[cookie_name] !== null) {
+        return JSON.parse(atob(localStorage[cookie_name]))
+    } else {
+        var c_start = c_value.indexOf(" " + cookie_name + "=");
+        if (c_start == -1) {
+            c_start = c_value.indexOf(cookie_name + "=");
+        }
+        if (c_start == -1) return false;
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = atob(unescape(c_value.substring(c_start, c_end)));
+        set_cookie("dimensionSave", player)
     }
-    if (c_start == -1) return false;
-    c_start = c_value.indexOf("=", c_start) + 1;
-    var c_end = c_value.indexOf(";", c_start);
-    if (c_end == -1) {
-        c_end = c_value.length;
-    }
-    c_value = atob(unescape(c_value.substring(c_start, c_end)));
     return JSON.parse(c_value, function(k, v) { return (v === Infinity) ? "Infinity" : v; });
 }
 
