8d12b934c5d1e2f4dfd002cbe46d91c1fa5ef023
IvarK <ivar.kerajarvi@aalto.fi>
2017-09-22 21:40:42 +0300

    time

--- javascripts/game.js
+++ javascripts/game.js
@@ -3188,13 +3188,13 @@ setInterval(function () {
 
     for (var i=0; i<=diff/50; i++) {
 
-    player.totalTimePlayed += 50
-    player.thisInfinityTime += 50
+    player.totalTimePlayed += 0.5
+    player.thisInfinityTime += 0.5
 
-    player.infinityPower = Decimal.max(player.infinityPower.times(getInfinityDimensionMultiplier(1, 5)), 1)
+    player.infinityPower = Decimal.max(player.infinityPower.times(getInfinityDimensionMultiplier(1, 0.05)), 1)
 
         for (var tier=1;tier<4;tier++) {
-            if (tier != 4 && player.infDimensionsUnlocked[tier-1]) player["infinityDimension"+tier].amount = player["infinityDimension"+tier].amount.times(Decimal.max((getInfinityDimensionMultiplier(tier+1, 5)), 1))
+            if (tier != 4 && player.infDimensionsUnlocked[tier-1]) player["infinityDimension"+tier].amount = player["infinityDimension"+tier].amount.times(Decimal.max((getInfinityDimensionMultiplier(tier+1, 0.05)), 1))
 
             if (player.infDimensionsUnlocked[tier-1]) {
                 document.getElementById("infRow"+tier).style.display = "inline-block"
