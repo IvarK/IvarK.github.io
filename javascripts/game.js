59362f1996f9069ff04d3604919545c823af4cfd
IvarK <ivar.kerajarvi@aalto.fi>
2017-09-23 07:55:27 +0300

    asscockshitdick

--- javascripts/game.js
+++ javascripts/game.js
@@ -1458,7 +1458,7 @@ function getInfinityDimensionMultiplier(tier, diff) {
     var dim = player["infinityDimension"+tier]
 
     var base = dim.amount.times(diff)
-    var timeVar = Decimal.pow((player.thisInfinityTime+1), getTimePow()).dividedBy(Decimal.pow(1.1, (dim.amount.times(1e-3).plus(1))))
+    var timeVar = Decimal.pow((player.thisInfinityTime+1), getTimePow()).dividedBy(Decimal.pow(1.1, (dim.amount.times(0.1).plus(1))))
     return timeVar = Decimal.pow(timeVar,base)
 
 }
