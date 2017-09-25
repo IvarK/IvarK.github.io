8ec6965f0a6a352265310720b4dcda821edef09e
IvarK <ivar.kerajarvi@aalto.fi>
2017-09-25 15:45:29 +0300

    wording change

--- javascripts/game.js
+++ javascripts/game.js
@@ -4205,7 +4205,7 @@ function playFabLoadCheckCallback(data, error) {
 		var playFabInfinitied = (data.data.Data.infinitied) ? parseInt(data.data.Data.infinitied.Value) : 0;
 		if (playFabInfinitied < player.infinitied){
             document.getElementById("loadCloud").style.display = "block";
-            document.getElementById("loadPopup").innerHTML = "You have a cloud save with "+playFabInfinitied+ " infinities. Do you want to load it?"
+            document.getElementById("loadPopup").innerHTML = "You have a cloud save with "+playFabInfinitied+ " infinities and your local save has "+player.infinitied+". Do you want to load it?"
 			return;
 		}
 		else loadFromPlayFab();
