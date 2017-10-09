4691639702e912584a795d13d33c8bd25aac8d45
Some One <1234abcdcba4321@users.noreply.github.com>
2017-10-09 11:23:20 -0500

    Update autobuyer.js

--- javascripts/autobuyer.js
+++ javascripts/autobuyer.js
@@ -13,13 +13,14 @@
 }*/
 
 
-var Autobuyer = function Autobuyer(target) {
+var Autobuyer = function Autobuyer(target,interval) {
     this.target = target
     this.cost = new Decimal(1)
-    this.interval = 5000;
+    this.interval = interval*1000
     this.priority = 1;
     this.ticks = 0;
     this.isOn = false;
-    this.tier = 1;
+    if (target < 9) this.tier = target;
+    else this.tier = 1; //i dont know what tier is used for but just in case, you know
     this.bulk = 1;
 }
