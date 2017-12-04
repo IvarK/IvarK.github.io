bce0fb22f1c4b8120656df374f3883bd226afd6c
IvarK <ivarkera@gmail.com>
2017-12-04 20:01:07 +0200

    more kong achs

--- javascripts/game.js
+++ javascripts/game.js
@@ -5223,6 +5223,7 @@ function eternity() {
             document.getElementById("replicantidiv").style.display="none"
             document.getElementById("replicantiunlock").style.display="inline-block"
         }
+        kongregate.stats.submit('Eternities', player.eternities);
         if (player.eternities > 2 && player.replicanti.galaxybuyer === undefined) player.replicanti.galaxybuyer = false
         document.getElementById("infinityPoints1").innerHTML = "You have <span class=\"IPAmount1\">"+shortenDimensions(player.infinityPoints)+"</span> Infinity points."
         document.getElementById("infinityPoints2").innerHTML = "You have <span class=\"IPAmount2\">"+shortenDimensions(player.infinityPoints)+"</span> Infinity points."
