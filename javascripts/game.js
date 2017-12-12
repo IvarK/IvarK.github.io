7b713d7d87b1622e4ec8ac34fd913ea9b5da41e3
Omsi <omsi2000@gmail.com>
2017-12-11 20:03:24 -0800

    removed pointless line of code

--- javascripts/game.js
+++ javascripts/game.js
@@ -6631,10 +6631,7 @@ function startInterval() {
             if (player.infinitied > 0) {
                 document.getElementById("infinitybtn").style.display = "inline-block";
                 document.getElementById("challengesbtn").style.display = "inline-block";
-            } else if (player.eternityChallUnlocked !== 0) {
-                document.getElementById("challengesbtn").style.display = "inline-block";
             }
-
         }
 
         document.getElementById("epmult").className = player.eternityPoints.gte(player.epmultCost) ? "eternityupbtn" : "eternityupbtnlocked"
