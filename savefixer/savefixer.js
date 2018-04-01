f8a83d196a55cd9f3a44db195e6f3054e7a76ed0
IvarK <ivarkera@gmail.com>
2018-04-01 18:02:18 +0300

    Revert "Fix merge conflicts"
    This reverts commit 16189e5543fb5d5b39a765a1627b47bd66a7bdcc, reversing
    changes made to 40df7d5aa238fd5dd8113c120b77f48a3431bfb6.

--- savefixer/savefixer.js
+++ savefixer/savefixer.js
@@ -2,6 +2,8 @@ function fixSave() {
   var save = atob(document.getElementById("brokenSave").value)
   
   var fixed = save.replace(/NaN/gi, "10")
+  var stillToDo = JSON.parse(fixed)
+  for (var i=0; i<fixed.autobuyers.length; i++) stillToDo.autobuyers[i].isOn = false
   
-  document.getElementById("fixed").value = btoa(fixed)
+  document.getElementById("fixed").value = btoa(stillToDo)
 }
