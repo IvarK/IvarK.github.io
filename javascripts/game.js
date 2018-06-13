8bfca4ac0d132e7dc2b3fc229e28893b2f2342bd
Omsi <omsi2000@gmail.com>
2018-06-13 11:57:32 -0700

    fix crunch animation toggle hiding after eternity

--- javascripts/game.js
+++ javascripts/game.js
@@ -4516,7 +4516,7 @@ setInterval(function() {
     if (player.infinitied > 0  || player.eternities > 0) document.getElementById("pastinfs").style.display = "inline-block"
     else document.getElementById("pastinfs").style.display = "none"
 
-    if (player.infinitied !== 0) document.getElementById("bigCrunchAnimBtn").style.display = "inline-block"
+    if (player.infinitied !== 0 || player.eternities !== 0) document.getElementById("bigCrunchAnimBtn").style.display = "inline-block"
     else document.getElementById("bigCrunchAnimBtn").style.display = "none"
     if (!player.dilation.tachyonParticles.eq(0)) document.getElementById("tachyonParticleAnimBtn").style.display = "inline-block"
     else document.getElementById("tachyonParticleAnimBtn").style.display = "none"
