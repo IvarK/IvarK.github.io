69c1995eadf0d0b989876beac8ccee569bb10c8b
yes* <divine.icbm@gmail.com>
2018-06-21 15:58:03 -0500

    fixed everythign everythign everythign everything

--- javascripts/howto.js
+++ javascripts/howto.js
@@ -87,7 +87,7 @@ function updateSpoilers() {
 			if (i === 17 && (player.replicanti.unl || player.eternities >= 1)) {
 				(displayed === 0) ? displayed = 1 : displayed = 0;
 			}
-			if (i === 18 && (player.eternities >= 1) {
+			if (i === 18 && (player.eternities >= 1)) {
 			    	(displayed === 0) ? displayed = 1 : displayed = 0;
 		}
 			if (i < 5 || i === 7) {
