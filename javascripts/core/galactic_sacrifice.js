6e5e4f92f6cd8e8086613b250bbbd6e83627a82b
Nyan cat <a0939712328@gmail.com>
2018-07-28 23:16:43 +0800

    Update galactic_sacrifice.js

--- javascripts/core/galactic_sacrifice.js
+++ javascripts/core/galactic_sacrifice.js
@@ -1,3 +1,9 @@
 function getGSAmount() {
     return Decimal.min(new Decimal(player.galaxies + player.replicanti.galaxies + player.dilation.freeGalaxies),player.eightAmount.div(50).floor())
 }
+
+function galacticSacrifice() {
+    player.infinityPoints = player.infinityPoints.add(getGSAmount())
+    player.galaxies = -1
+    galaxyReset()
+}
