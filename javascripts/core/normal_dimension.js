d680c9f0865854844b817af30330b83208eea755
Nyan cat <a0939712328@gmail.com>
2018-08-04 11:37:33 +0800

    Update normal_dimension.js

--- javascripts/core/normal_dimension.js
+++ javascripts/core/normal_dimension.js
@@ -575,9 +575,9 @@ function getDimensionProductionPerSecond(tier) {
     if (player.currentChallenge == "challenge2" || player.currentChallenge == "postc1") ret = ret.times(player.chall2Pow)
  // if (player.dilation.active) {
         let tick = new Decimal(player.tickspeed)
-        tick = Decimal.pow(10, Math.pow(Math.max(3,Math.abs(tick.log10())), 0.75))
+        tick = Decimal.pow(10, Math.pow((tick.log10()*-1)+3, 0.75))
         if (player.dilation.upgrades.includes(9)) {
-            tick = Decimal.pow(10, Math.pow(Math.max(3,Math.abs(tick.log10())), 1.05))
+            tick = Decimal.pow(10, Math.pow((tick.log10()*-1)+3, 1.05))
           }
         tick = new Decimal(1).dividedBy(tick)
         ret = Decimal.floor(player[TIER_NAMES[tier] + 'Amount']).times(getDimensionFinalMultiplier(tier)).times(1000).dividedBy(tick)
