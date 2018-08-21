911eed2fe4e523bb5058d85e6b3caa729a3a946e
pg132 <42481851+pg132@users.noreply.github.com>
2018-08-21 07:14:01 -0700

    Adding a reward to IC1 that boosts gp.

--- javascripts/core/galactic_sacrifice.js
+++ javascripts/core/galactic_sacrifice.js
@@ -1,6 +1,8 @@
 function getGSAmount() {
   let galaxies = player.galaxies + player.replicanti.galaxies + player.dilation.freeGalaxies;
-  let ret = new Decimal(Math.max(Math.pow(galaxies,1.5) * (player.resets - (player.currentChallenge=="challenge4"?2:4)), 0));
+  let y = 1.5 + Math.max(0, 0.01*(galaxies - 10))
+  if (player.challenges.includes("postc1") y = 1.5
+  let ret = new Decimal(Math.max(Math.pow(galaxies, y) * (player.resets - (player.currentChallenge=="challenge4"?2:4)), 0));
   ret = ret.times(1 + player.eightAmount/50)
   if (player.galacticSacrifice.upgrades.includes(32)) {
     ret = ret.times(galUpgrade32());
