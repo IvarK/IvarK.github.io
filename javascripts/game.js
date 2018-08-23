7d847d5ce667437d24a27cd1346150ca6cc8eae0
e666666 <a0939712328@gmail.com>
2018-08-23 14:54:43 +0800

    fixed dimBoolean's WTFness

--- javascripts/game.js
+++ javascripts/game.js
@@ -5830,7 +5830,7 @@ function dimBoolean() {
     var name = TIER_NAMES[getShiftRequirement(0).tier]
     if (!player.autobuyers[9].isOn) return false
     if (player.autobuyers[9].ticks*100 < player.autobuyers[9].interval) return false
-    if (player[name + "Amount"] > getShiftRequirement(0)) return true
+    if (player[name + "Amount"].gte(getShiftRequirement(0).amount)) return true
     if (player.eternities < 10 && player[name + "Amount"] < getShiftRequirement(player.autobuyers[9].bulk-1).amount) return false
     if (player.overXGalaxies <= player.galaxies) return true
     if ((player.currentChallenge =="challenge4" || player.currentChallenge == "postc4") && player.autobuyers[9].priority < getShiftRequirement(0).amount && getShiftRequirement(0).tier == 6) return false
