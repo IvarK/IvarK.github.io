b70abdec61a67c34d121177253a8f7d36e588cb7
Andrei Andreev <Razenpok@outlook.com>
2018-09-15 02:32:02 +0200

    Fix Kongregate integration
    1. All Kong stuff was extracted to kong.js and wrapped in kong object
    2. Kong and PlayFab will not be called unless we are hosted inside
    Kongregate IFrame (document.referrer checks for that)
    
    Resolves: #243

--- javascripts/core/kong.js
+++ javascripts/core/kong.js
@@ -1,52 +1,67 @@
-function purchaseIP() {
-  console.log("purchase ip")
-  kongregate.mtx.purchaseItems(['doubleip'], onPurchaseResult)
-}
+var kong = {};
 
-function purchaseDimMult() {
-  kongregate.mtx.purchaseItems(['doublemult'], onPurchaseResult)
-}
+kong.enabled = false;
 
-function purchaseAllDimMult() {
-  kongregate.mtx.purchaseItems(['alldimboost'], onPurchaseResult)
-}
+kong.init = function() {
+    if (document.referrer.indexOf("kongregate") === -1)
+        return;
+    kong.enabled = true;
+    try {
+        kongregateAPI.loadAPI(function () {
+            window.kongregate = kongregateAPI.getAPI();
+            kong.updatePurchases();
+        });
+    } catch (err) { console.log("Couldn't load Kongregate API") }
+};
 
+kong.submitStats = function(name, value) {
+    if (!kong.enabled) return;
+    try {
+        kongregate.stats.submit(name, value);
+    } catch(e) { console.log(e) }
+};
 
-function purchaseTimeSkip() {
-  kongregate.mtx.purchaseItems(['timeskip'], onPurchaseTimeSkip)
-}
+kong.purchaseIP = function() {
+    console.log("purchase ip");
+    kongregate.mtx.purchaseItems(['doubleip'], kong.onPurchaseResult);
+};
 
-function purchaseEP() {
-  kongregate.mtx.purchaseItems(['tripleep'], onPurchaseResult)
-}
+kong.purchaseDimMult = function() {
+    kongregate.mtx.purchaseItems(['doublemult'], kong.onPurchaseResult);
+};
 
+kong.purchaseAllDimMult = function() {
+    kongregate.mtx.purchaseItems(['alldimboost'], kong.onPurchaseResult);
+};
 
-function onPurchaseResult(result) {
-  console.log("purchasing...")
-  if (result.success) {
-      console.log("purchase successfull!")
-      updateKongPurchases()
-  }
-}
+kong.purchaseTimeSkip = function() {
+    kongregate.mtx.purchaseItems(['timeskip'], kong.onPurchaseTimeSkip);
+};
 
+kong.purchaseEP = function() {
+    kongregate.mtx.purchaseItems(['tripleep'], kong.onPurchaseResult);
+};
 
-function onPurchaseTimeSkip(result) {
-  if (result.success) {
-      simulateTime(21600)
-  }
-}
+kong.onPurchaseResult = function(result) {
+    console.log("purchasing...");
+    if (result.success) {
+        console.log("purchase successfull!");
+        kong.updatePurchases();
+    }
+};
 
+kong.onPurchaseTimeSkip = function(result) {
+    if (result.success) {
+        simulateTime(21600);
+    }
+};
 
-
-
-
-
-function updateKongPurchases() {
-  console.log("updating kong purchases")
+kong.updatePurchases = function() {
+  if (!kong.enabled) return;
+  console.log("updating kong purchases");
   try {
       kongregate.mtx.requestUserItemList("", items)
-
-  } catch(e) {console.log(e)}
+  } catch(e) { console.log(e) }
 
   function items(result) {
       console.log("checking for all items")
@@ -76,4 +91,4 @@ function updateKongPurchases() {
   document.getElementById("kongep").textContent = "Triple your EP gain from all sources (additive). Forever. Currently: x"+kongEPMult+", next: x"+(kongEPMult==1? 3: kongEPMult+3)
   document.getElementById("kongdim").textContent = "Double all your normal dimension multipliers (multiplicative). Forever. Currently: x"+kongDimMult+", next: x"+(kongDimMult*2)
   document.getElementById("kongalldim").textContent = "Double ALL the dimension multipliers (Normal, Infinity, Time) (multiplicative until 32x). Forever. Currently: x"+kongAllDimMult+", next: x"+((kongAllDimMult < 32) ? kongAllDimMult * 2 : kongAllDimMult + 32)
-}
+};
