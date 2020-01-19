c64a3ec624da07875ac6e78db1655547c7a2927a
Benjamin Steinwender <auge@users.noreply.github.com>
2020-01-19 22:06:38 +0100

    fixed spelling (#261)
    uppercase words look weird in this sentence

--- javascripts/game.js
+++ javascripts/game.js
@@ -4937,7 +4937,7 @@ function gameLoop(diff) {
 
 
     var estimate = Math.max((Math.log(Number.MAX_VALUE) - current) / est, 0)
-    document.getElementById("replicantiapprox").textContent ="Approximately "+ timeDisplay(estimate*10) + " Until Infinite Replicanti"
+    document.getElementById("replicantiapprox").textContent ="Approximately "+ timeDisplay(estimate*10) + " until infinite Replicanti"
 
     document.getElementById("replicantiamount").textContent = shortenDimensions(player.replicanti.amount)
     var replmult = Decimal.pow(Decimal.log2(Decimal.max(player.replicanti.amount, 1)), 2)
